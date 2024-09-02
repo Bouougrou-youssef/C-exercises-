57d90c7abf941556a168eaeb6fdee2c222f33de4
Bouougrou-youssef <dealwith.youssef@gmail.com>
2024-09-02 23:34:10 +0100

    added challenges from 1 to 9

---
+++ Challenge 2  Conversion de la température.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+int main ()
+{
+
+float C,K ;
+printf("enter temperature in C :");
+scanf("%f",&C);
+K = C+273.15;
+printf("temperature in K %f ",K);
+
+return 0 ;
+
+}
+
