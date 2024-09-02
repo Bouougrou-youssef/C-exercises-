175bc9fe6551f12d13df6fead1e8fe346a461afa
Bouougrou-youssef <dealwith.youssef@gmail.com>
2024-09-03 00:06:37 +0100

    modified challenges 2 and 3

--- Challenge 3  Conversion de la distance.c
+++ Challenge 3  Conversion de la distance.c
@@ -1,16 +1,11 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <math.h>
 int main ()
 {
-
 float km,yards ;
 printf("enter distance in km  :");
 scanf("%f",&km);
 yards = km * 1093.61;
 printf("distance in yards %f ",yards);
-
 return 0 ;
-
 }
 
